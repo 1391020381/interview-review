dc6922ff1ee8f1f9e087215338424e4051220490
1391020381 <1391020381@qq.com>
2022-03-03 20:48:52 +0800

    moudle

--- js-basic/module/js/b.js
+++
@@ -1,8 +0,0 @@
-// define('./js/b', function (require, exports, module) {
-//     var a = require('./a');
-//     var b = a + 1;
-//     module.exports = b;
-// });
-define('./js/b', function (require, exports, module) {
-    return { c: 3, d: 4 }
-});
