6fa5a7164fc3c5ed784e0d83e0dd9c0195884844
1391020381 <1391020381@qq.com>
2024-09-12 10:13:59 +0800

    feat: vue3-challenges

---
+++ Vue3/vue3-challenges/src/pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+    about
+</template>
+<script setup lang="ts">
+console.log('about')
+</script>
+<style lang="less" scoped></style>
---
+++ Vue3/vue3-challenges/src/pages/home.vue
@@ -0,0 +1,7 @@
+<template>
+    home
+</template>
+<script setup lang="ts">
+console.log('home')
+</script>
+<style lang="less" scoped></style>
